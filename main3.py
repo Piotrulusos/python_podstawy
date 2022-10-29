705d7678ff93486068da6a09fcb447124de4cc05
Piotrulusos <115098591+Piotrulusos@users.noreply.github.com>
2022-10-29 19:33:03 +0200

    3. multiple_assignment

---
+++ main3.py
@@ -0,0 +1,24 @@
+# multiple assignment = allows us to assign multiple variables at the same time in one line of code
+# wielokrotne przypisanie = pozwala nam przypisać wiele zmiennych jednocześnie w jednej linii kodu
+
+# name = "Piotr"
+# age = 19
+# attractive = True
+
+# name, age, attractive = "Piotr", 19, True
+
+# print(name)
+# print(age)
+# print(attractive)
+
+# Spongebob = 30
+# Patrick = 30
+# Sandy = 30
+# Squidward = 30
+
+Spongebob = Patrick = Sandy = Squidward = 30
+
+print(Spongebob)
+print(Patrick)
+print(Sandy)
+print(Squidward)
