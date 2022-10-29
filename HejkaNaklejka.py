949f3d207676006284dbe4a1d583c6f07594d031
Piotrulusos <115098591+Piotrulusos@users.noreply.github.com>
2022-10-29 18:40:07 +0200

    variables

---
+++ HejkaNaklejka.py
@@ -0,0 +1,21 @@
+# variable = a container for value. Behaves as the value that it contains
+# zmienna = pojemnik na wartość. Zachowuje się jak wartość, którą zawiera
+
+first_name = "Piotr"
+last_name = "Kowalczyk"
+full_name = first_name + " " + last_name
+print("Hello "+full_name)
+# print(type(name))
+
+age = 19
+age += 1
+print("Your age is: "+str(age))
+# print(type(age))
+
+height = 250.5
+print("Your height is: "+str(height)+"cm")
+# print(type(height))
+
+human = True
+print("Are you a human: "+str(human))
+# print(type(human))
