cdd0b565c0958f30fd8f2311fbc12d1f4c9069b1
Piotrulusos <115098591+Piotrulusos@users.noreply.github.com>
2022-10-29 20:22:36 +0200

    5. type_cast

---
+++ main5.py
@@ -0,0 +1,15 @@
+# type casting = convert the data type of a value to another data type
+# rzutowanie typu = konwersja typu danych wartości na inny typ danych
+
+x = 1    # int
+y = 2.0  # float
+z = "3"  # str
+
+x = str(x)
+y = str(y)
+z = str(z)
+
+print(x)
+print(y)
+print(z*3)
+
