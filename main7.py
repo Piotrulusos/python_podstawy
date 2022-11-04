dba962e37c3fdfbb2746f3dcc3430b30f0ed7171
Piotrulusos <115098591+Piotrulusos@users.noreply.github.com>
2022-11-04 20:42:19 +0100

    7. math_functions

---
+++ main7.py
@@ -0,0 +1,15 @@
+import math
+
+pi = -3.14
+x = 1
+y = 2
+z = 3
+
+# print(round(pi))         zaokraglanie zwykle
+# print(math.ceil(pi))     zaokraglanie w górę do najblizszej liczby calkowitej
+# print(math.floor(pi))    zaokraglanie w dol -||-
+# print(abs(pi)) jak       jak daleko jest od zera
+# print(pow(pi,2))         potegowanie
+print(math.sqrt(420))    # pierwiastkowanie
+# print(max(x,y,z))        najwieksza wartosc
+# print(min(x,y,z))        najmniejsza wartosc
